98648432efadc08bc17032da6119756dd5b0e36c
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-04-17 10:56:06 +0200

    Update ApplicationDbContext.cs

--- Assignment3/Data/ApplicationDbContext.cs
+++ Assignment3/Data/ApplicationDbContext.cs
@@ -13,5 +13,15 @@ namespace Assignment3.Data
         public DbSet<Assignment3.Models.Reservation> Reservation { get; set; } = default!;
         public DbSet<Assignment3.Models.CheckInd> CheckInd { get; set; } = default!;
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Reservation>()
+                .HasKey(r => r.ReservationId);
+            modelBuilder.Entity<CheckInd>()
+                .HasKey(c => c.CheckIndId);
+        }
     }
 }
